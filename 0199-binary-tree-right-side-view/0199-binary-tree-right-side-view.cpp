d52e3a3d28db506288804f47c7af63c3f7e534b7
ANWESHA JHA <anwesshajhaa@gmail.com>
2025-12-25 23:10:13 +0530

    Time: 0 ms (100%), Space: 14.7 MB (99.58%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -13,8 +13,8 @@ class Solution {
 public:
 void solve(TreeNode* root,vector<int>&ans,int level){
     if(root==NULL)return;
-
-    if(level==ans.size())ans.push_back(root->val);
+    if(level==ans.size())
+    ans.push_back(root->val);
     solve(root->right,ans,level+1);
     solve(root->left,ans,level+1);
 }
