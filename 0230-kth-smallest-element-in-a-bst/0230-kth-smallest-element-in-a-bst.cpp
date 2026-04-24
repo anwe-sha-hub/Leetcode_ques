4cd9f0f804018f769226f78797891d353e844fa7
ANWESHA JHA <anwesshajhaa@gmail.com>
2026-04-24 13:44:24 +0530

    Time: 0 ms (100%), Space: 24.2 MB (89.61%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,25 +6,48 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-int solve(TreeNode* root, int &i,int k){
-    if(root==NULL) return -1;
-    //l
-    int leftans=solve(root->left,i,k);
-    if(leftans!=-1) return leftans;i++;
-//n
-if(i==k) return root->val;
+    // using recursion
+    //  int solve(TreeNode* root, int &i,int k){
+    //      if(root==NULL) return -1;
+    //      //l
+    //      int leftans=solve(root->left,i,k);
+    //      if(leftans!=-1) return leftans;i++;
+    //  //n
+    //  if(i==k) return root->val;
 
-//r
-return solve(root->right,i,k);
-}
+    // //r
+    // return solve(root->right,i,k);
+    // }
     int kthSmallest(TreeNode* root, int k) {
-        int i=0;
-        int ans=solve(root,i,k);
-        return ans;
+        TreeNode* curr = root;
+        int c = 0;
+        int res=-1;
+        while (curr) {
+            if (curr->left == NULL) {
+                c++;
+                if (c == k) res=curr->val;
+                curr = curr->right;
+            } else {
+                TreeNode* prev = curr->left;
+                while (prev->right && prev->right != curr)
+                    prev = prev->right;
+                if (prev->right == NULL) {
+                    prev->right = curr;
+                    curr = curr->left;
+                } else {
+                    prev->right = NULL;
+                    c++;
+                    if (c == k) res= curr->val;
+                    curr = curr->right;
+                }
+            }
+        }
+        return res;
     }
 };
