1dbea8b83721af604a1a14840b8211ab81fdb791
ANWESHA JHA <anwesshajhaa@gmail.com>
2026-05-01 18:04:27 +0530

    Time: 0 ms (100%), Space: 8.4 MB (14.09%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int countA=0;
+        int countL=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='A'){
+                countA++;
+                if(countA>=2) return false;
+                countL=0;
+            }
+            if(s[i]=='L'){
+                countL++;
+                if(countL>=3) return false;
+            }
+            else{
+                countL=0;
+            }
+        }
+        return true;
+    }
+};
