218fc139c2f74e9fe97abb3559d36b49ae20f6e3
ANWESHA JHA <anwesshajhaa@gmail.com>
2026-05-06 11:34:28 +0530

    Time: 0 ms (100%), Space: 9.4 MB (39%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+        //frequence updated
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='a'){
+                lower[s[i]-'a']++;
+            }
+            else upper[s[i]-'A']++;
+        }
+
+        int c=0;
+        bool odd=0;
+
+        for(int i=0; i< 26;i++){
+            //lower
+            if(lower[i]%2==0){
+                c+=lower[i];
+            }
+            else{
+                c+=lower[i]-1;
+                odd=1;
+            }
+            //upper
+            if(upper[i]%2==0){
+                c+=upper[i];
+            }
+            else{
+                c+=upper[i]-1;
+                odd=1;
+            }
+        }
+        return c+odd;
+    }
+};
