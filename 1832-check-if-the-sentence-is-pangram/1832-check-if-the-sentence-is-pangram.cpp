7c4bfed382c4e13c7b2b4e483786504180430304
ANWESHA JHA <anwesshajhaa@gmail.com>
2026-06-09 18:58:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (83.74%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -7,11 +7,9 @@ public:
                 freq[sentence[i] - 'a'] = 1;
             }
         }
-
         for(int i = 0; i < 26; i++){
             if(freq[i] == 0) return false;
         }
-
         return true;
     }
 };
