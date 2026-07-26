ab09c18d21c409d707651d0b18ec7308c91b00c5
ANWESHA JHA <anwesshajhaa@gmail.com>
2026-07-26 23:07:39 +0530

    Time: 0 ms (100%), Space: 14.1 MB (59.17%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+void fun(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+    if(ind==arr.size()){
+        if(target==0) ans.push_back(ds);
+        return;
+    }
+    if(arr[ind]<=target){
+        ds.push_back(arr[ind]);
+        //pick
+        fun(ind,target-arr[ind],arr,ans,ds);
+        ds.pop_back();
+    }
+    fun(ind+1,target,arr,ans,ds);
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        fun(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
